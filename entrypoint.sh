bd48ce784e5fcb5be67af61897e233b17e83b055
cazadorDeColombianos <90215784+drakexcaptainop@users.noreply.github.com>
2025-11-10 18:30:45 -0400

    fix 12

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,4 +34,4 @@ sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-available/000-default.conf
 
 echo "Starting Apache..."
-exec apache2-foreground
+exec /usr/sbin/apache2ctl -D FOREGROUND
